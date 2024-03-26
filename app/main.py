b1553cecf71c9c2c5b8dbf761df088832119ea77
Dhruv Ajay Ray <dar10@iitbbs.ac.in>
2024-03-26 15:25:18 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,14 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    # server_socket.accept() # wait for client
+    
+    pong = "+PONG\r\n"
+    connection, address = server_socket.accept()
+    with connection:
+        connection.recv(1024)
+        connection.send(pong.encode())
+        
 
 
 if __name__ == "__main__":
