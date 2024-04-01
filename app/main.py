ab86eb0b9ea9fe1c5eec5d3fc99dc8b3e01234cd
Dhruv Ajay Ray <dar10@iitbbs.ac.in>
2024-04-01 11:37:48 +0530

    info replicate

--- app/main.py
+++ app/main.py
@@ -86,6 +86,7 @@ def handleRequest(connection):
                 connection.send(response) 
             elif commands[0][0] == "INFO":
                 if replica_server:
+                    print("replicate comm sent")
                     connection.send("$10\r\nrole:slave\r\n".encode())    
                 else:
                     connection.send("$11\r\nrole:master\r\n".encode())   
