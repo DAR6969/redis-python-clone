89e7513447d9f0c2842c457a46ab02c4616371fa
Dhruv Ray <dhruvray@Dhruvs-Laptop.local>
2024-11-27 16:24:19 +0530

    new folders 3

--- app/common_file.py
+++ app/common_file.py
@@ -1,12 +1,12 @@
 class CommonTools:
     def __init__(self) -> None:
-        self.my_local_port = ""
-        self.ping = "PING"
-        self.REPLCONF_port = "REPLCONF listening-port "
-        self.REPLCONF_capa = "REPLCONF capa psync2"
-        self.psync = "PSYNC ? -1"
-        self.master_host = ""
-        self.master_port = 0
+        my_local_port = ""
+        ping = "PING"
+        REPLCONF_port = "REPLCONF listening-port "
+        REPLCONF_capa = "REPLCONF capa psync2"
+        psync = "PSYNC ? -1"
+        master_host = ""
+        master_port = 0
 
     def set_my_port(self, port):
         self.my_local_port = str(port)
