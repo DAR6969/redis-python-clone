f1c443eca7abf003ffd3c665407992f6ab0b4162
Dhruv Ray <dhruvray@Dhruvs-Laptop.local>
2024-11-30 00:40:01 +0530

    new folder

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ def main():
     args = parse_arguments()
     master = args.master
     if master is not None:
-        CommonTools.set_replica_server(True)
+        CommonTools.set_replica_server(value=True)
         master = master[0].split()
         print(master)
         common_tools.set_master_addr(master[0], master[1])
